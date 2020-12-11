64dfb50336875cc1a87ab5a6d02a181a922efb57
Rahul Kumar <RahulKumar5274@gmail.com>
2020-12-11 16:47:22 +0000

    v1.1

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function winner()
 }
 else if(randomNumber1 === randomNumber2)
 {
-  document.querySelector("h1").innerHTML= "🍕🍕🍕 Draw!";
+  document.querySelector("h1").innerHTML= "🍕 Draw! 🍕";
 }else{
-  document.querySelector("h1").innerHTML= "🥂 Player 2 won!";
+  document.querySelector("h1").innerHTML= "Player 2 won! 🥂";
 }
 }
 
