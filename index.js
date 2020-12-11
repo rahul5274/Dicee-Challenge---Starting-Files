ba1119e150ef7f3d131b31e4886b91eead85a55b
Rahul Kumar <RahulKumar5274@gmail.com>
2020-12-11 16:46:13 +0000

    v1.1

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+"
 function winner()
 {
   if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML= "Player 1 won!";
+  document.querySelector("h1").innerHTML= "🥂 Player 1 won!";
 }
 else if(randomNumber1 === randomNumber2)
 {
-  document.querySelector("h1").innerHTML= "Draw!";
+  document.querySelector("h1").innerHTML= "🍕🍕🍕 Draw!";
 }else{
-  document.querySelector("h1").innerHTML= "Player 2 won!";
+  document.querySelector("h1").innerHTML= "🥂 Player 2 won!";
 }
 }
 
