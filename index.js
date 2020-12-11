8817b5343d611fb5fc8cf13c7fdc32183a23cd49
Rahul Kumar <RahulKumar5274@gmail.com>
2020-12-11 16:34:51 +0000

    v1

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+function winner()
+{
+  if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML= "Player 1 won!";
+}
+else if(randomNumber1 === randomNumber2)
+{
+  document.querySelector("h1").innerHTML= "Draw!";
+}else{
+  document.querySelector("h1").innerHTML= "Player 2 won!";
+}
+}
+
+winner();
